a4332e447824bc30b9e1c63efb3e2daec605d92b
Sergey Sheyko <sheikos1991@gmail.com>
2016-02-06 15:02:27 +0300

    Implemented insert method.

--- binary-tree.js
+++ binary-tree.js
@@ -6,7 +6,28 @@ class BinaryTree {
 	}
 
 	insert(data) {
+        var currentNode = this.root, parentNode = null;
 
+        while (currentNode != null) {
+            parentNode = currentNode;
+            if (data < currentNode.data) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+
+        var newNode = new Node(data, null, null);
+
+        if (parentNode == null) {
+            this.root = newNode;
+        } else {
+            if (data < parentNode.data) {
+                parentNode.left = newNode;
+            } else {
+                parentNode.right = newNode;
+            }
+        }
 	}
 
 	contains(data) {
