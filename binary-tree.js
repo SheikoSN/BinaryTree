8714a9e1adf86bbb375f77c13cc0aba89f141c78
Sergey Sheyko <sheikos1991@gmail.com>
2016-02-06 15:09:11 +0300

    Implemented remove method.

--- binary-tree.js
+++ binary-tree.js
@@ -49,7 +49,52 @@ class BinaryTree {
 	}
 
 	remove(data) {
+        var currentNode = this.root, parentNode = null;
 
+        while (currentNode != null) {
+            if (data == currentNode.data) {
+                break;
+            }
+
+            parentNode = currentNode;
+            if (data < currentNode.data) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+
+        if (currentNode == null) {
+            return;
+        } else if (parentNode == null) {
+            this.root = null;
+        } else if (currentNode.left == null && currentNode.right == null) {
+            Utils.changeDirectChildNode(parentNode, data, null);
+        } else if (currentNode.left == null || currentNode.right == null) {
+            if (currentNode.left == null) {
+                Utils.changeDirectChildNode(parentNode, data, currentNode.right);
+            } else {
+                Utils.changeDirectChildNode(parentNode, data, currentNode.left);
+            }
+        } else {
+            var x = currentNode.right, parentX = currentNode;
+
+            while (x.left != null) {
+                parentX = x;
+                x = x.left;
+            }
+
+            if (parentX != currentNode) {
+                parentX.left = x.right;
+            } else {
+                parentX.right = x.right;
+            }
+
+            x.left = currentNode.left;
+            x.right = currentNode.right;
+
+            Utils.changeDirectChildNode(parentNode, data, x);
+        }
 	}
 
 	size() {
@@ -59,4 +104,14 @@ class BinaryTree {
 	isEmpty() {
 
 	}
+}
+
+class Utils {
+    static changeDirectChildNode(parentNode, data, changeNode) {
+        if (data < parentNode.data) {
+            parentNode.left = changeNode;
+        } else {
+            parentNode.right = changeNode;
+        }
+    }
 }
